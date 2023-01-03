2adcbd8c28a3697f017a9cac034e941461a8d02e
gbenba <gbenba13@gmail.com>
2023-01-02 21:42:08 -0300

    Incorporación de primeros archivos

---
+++ app.js
@@ -0,0 +1,35 @@
+const $container = document.getElementById("container");
+const URL_API = "https://rickandmortyapi.com/api/character";
+//
+// console.log(URL_API); 
+//
+fetch(URL_API)
+  .then((response) => response.json())
+  .then((data) => {
+    const characters = data.results;
+
+    for (let character of characters) {
+      //
+      // console.log(character);
+      //
+      $container.innerHTML += `
+      <div class="option-name"> <img class="character-img" src="${character.image}" 
+          alt="imagen de ${character.name}"> 
+          <br> ${character.id}. 
+          <br> Name: ${character.name} 
+          <br> Gender: ${character.gender} 
+          <br> Species: ${character.species} 
+      </div>
+    `;
+    }
+  });
+
+
+  // Nota: Asi lo vimos en clase
+  //
+  //   $container.innerHTML += `
+  //     <li class="option-name"> <img class="character-img" src="${character.image}" 
+  //         alt="imagen de ${character.name}"> ${character.name} - ${character.status}
+  //     </li>
+  //   `;
+  
